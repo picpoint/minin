82f372b8bdb9c59ecb843c0a47fa95682d780133
rmtar <picpoint@yandex.ru>
2019-11-13 23:55:59 +0300

    work with express

--- app.js
+++ app.js
@@ -1,53 +1,21 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
 const path = require('path');
+const app = express();
+const port = process.env.port || 4000;
+
+app.use(express.static(path.join('public')));
 
 
-http.createServer(function(request, response) {
-  if(request.method == 'GET') {
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    if(request.url == '/') {
-      fs.readFile(path.join(__dirname, 'views', 'index.html'), 'utf-8', (err, content) => {
-        if(err) {
-          throw new Error('***ERR to view page index***');
-        } else {
-          response.end(content);
-        }
-      });
-    } else if(request.url == '/about') {
-      fs.readFile(path.join(__dirname, 'views', 'about.html'), 'utf-8', (err, content) => {
-        if(err) {
-          throw new Error('***ERR to view page index***');
-        } else {
-          response.end(content);
-        }
-      });
-    } else if(request.url == '/api/users') {
-      response.writeHead(200, {'Content-Type': 'text/json'});
-      let users = [
-        {name: 'Semen', age: 20},
-        {name: 'Igor', age: 40}
-      ];
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/index.html'));
+});
 
-      response.end(JSON.stringify(users));
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/about.html'));
+});
 
-    }
-  } else if(request.method == 'post') {    
-    const body = [];
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    request.on('data', (data) => {
-      body.push(Buffer.from(data));
-    });
 
-    request.on('end', () => {
-      let message = body.toString().split('-')[1];      
-    });
 
-    response.end(`
-      <h1>Ваше сообщение-${message}</h1>
-    `);
-  }
-
-}).listen(4000, function() {
-  console.log('server is starting');
+app.listen(port, () => {
+  console.log(`---server starting on port ${port}---`);
 });
