c83e81583d6df109e0fb89dc7a81485e4149a384
picpoint <picpoint@yandex.ru>
2019-11-13 16:51:48 +0300

    work with native nodejs

--- app.js
+++ app.js
@@ -1,16 +1,53 @@
-const express = require('express');
-const app = express();
-const port = process.env.port || 4000;
-const obj = require('./modules/obj');
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');  
-  console.log(`__dirname - ${__dirname}`);
-  console.log(obj.truobj.name);  
-  obj.sayHello();
-});
+http.createServer(function(request, response) {
+  if(request.method == 'GET') {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    if(request.url == '/') {
+      fs.readFile(path.join(__dirname, 'views', 'index.html'), 'utf-8', (err, content) => {
+        if(err) {
+          throw new Error('***ERR to view page index***');
+        } else {
+          response.end(content);
+        }
+      });
+    } else if(request.url == '/about') {
+      fs.readFile(path.join(__dirname, 'views', 'about.html'), 'utf-8', (err, content) => {
+        if(err) {
+          throw new Error('***ERR to view page index***');
+        } else {
+          response.end(content);
+        }
+      });
+    } else if(request.url == '/api/users') {
+      response.writeHead(200, {'Content-Type': 'text/json'});
+      let users = [
+        {name: 'Semen', age: 20},
+        {name: 'Igor', age: 40}
+      ];
 
-app.listen(port, () => {
-  console.log(`--- server start on port ${port} ---`);
+      response.end(JSON.stringify(users));
+
+    }
+  } else if(request.method == 'post') {    
+    const body = [];
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    request.on('data', (data) => {
+      body.push(Buffer.from(data));
+    });
+
+    request.on('end', () => {
+      let message = body.toString().split('-')[1];      
+    });
+
+    response.end(`
+      <h1>Ваше сообщение-${message}</h1>
+    `);
+  }
+
+}).listen(4000, function() {
+  console.log('server is starting');
 });
